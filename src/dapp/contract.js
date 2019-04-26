f60cec13d8f8c7749a02e476767cf2bde4785ce8
Harvey <harvey.leung@me.com>
2019-04-27 00:34:27 +0800

    First attempt in FlightSurety Project

--- src/dapp/contract.js
+++ src/dapp/contract.js
@@ -1,4 +1,5 @@
 import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
+
 import Config from './config.json';
 import Web3 from 'web3';
 
@@ -7,7 +8,10 @@ export default class Contract {
 
         let config = Config[network];
         this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
+
+       // this.flightSuretyData = new this.web3.eth.Contract(FlightSuretyData.abi, config.dataAddress);
         this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+
         this.initialize(callback);
         this.owner = null;
         this.airlines = [];
@@ -16,11 +20,11 @@ export default class Contract {
 
     initialize(callback) {
         this.web3.eth.getAccounts((error, accts) => {
-           
+
             this.owner = accts[0];
 
             let counter = 1;
-            
+
             while(this.airlines.length < 5) {
                 this.airlines.push(accts[counter++]);
             }
@@ -29,13 +33,15 @@ export default class Contract {
                 this.passengers.push(accts[counter++]);
             }
 
+            //Initial for 5 airlines,
+
             callback();
         });
     }
 
     isOperational(callback) {
-       let self = this;
-       self.flightSuretyApp.methods
+        let self = this;
+        self.flightSuretyApp.methods
             .isOperational()
             .call({ from: self.owner}, callback);
     }
@@ -46,11 +52,56 @@ export default class Contract {
             airline: self.airlines[0],
             flight: flight,
             timestamp: Math.floor(Date.now() / 1000)
-        } 
+        }
         self.flightSuretyApp.methods
             .fetchFlightStatus(payload.airline, payload.flight, payload.timestamp)
             .send({ from: self.owner}, (error, result) => {
                 callback(error, payload);
             });
     }
+
+    registerFlight(flight, value, callback) {
+        let self = this;
+        let flightNumber = flight;
+        let flightTime =  Math.floor(Date.now() / 1000)
+       // let flightTime = 5678;
+        console.log("Going to send to airline = " + self.airlines[0] + " passengers = " + self.passengers[0]);
+        console.log("Flight " + flightNumber  + " timestamp=" + flightTime);
+
+        self.flightSuretyApp.methods
+            .registerFlight(self.airlines[0], flightNumber,flightTime )
+            .send({from: self.passengers[0], value: self.web3.utils.toWei(value, "ether"), gas:3000000 }, (error, result) => {
+                if (error) {
+                    console.log("ERROR:");
+                    console.log(error);
+                } else {
+                    console.log(result);
+                    callback(result);
+                }
+            });
+    } //end registerFlight
+
+    oracleReport(callback) {
+        let self = this;
+        self.flightSuretyApp.events.OracleReport({}, function (error, event) {
+            if (error) {
+                console.log(error);
+            } else {
+                callback(event.returnValues);
+            }
+        })
+    }
+
+    flightStatusInfo(callback) {
+        let self = this;
+        self.flightSuretyApp.events.FlightStatusInfo({}, function (error, event) {
+            if (error) {
+                console.log(error);
+            } else {
+                callback(event.returnValues);
+            }
+        })
+    }
+
+
 }
--- src/dapp/index.js
+++ src/dapp/index.js
@@ -1,4 +1,3 @@
-
 import DOM from './dom';
 import Contract from './contract';
 import './flightsurety.css';
@@ -10,12 +9,16 @@ import './flightsurety.css';
 
     let contract = new Contract('localhost', () => {
 
+
+
         // Read transaction
         contract.isOperational((error, result) => {
             console.log(error,result);
             display('Operational Status', 'Check if contract is operational', [ { label: 'Operational Status', error: error, value: result} ]);
         });
-    
+
+
+
 
         // User-submitted transaction
         DOM.elid('submit-oracle').addEventListener('click', () => {
@@ -25,9 +28,35 @@ import './flightsurety.css';
                 display('Oracles', 'Trigger oracles', [ { label: 'Fetch Flight Status', error: error, value: result.flight + ' ' + result.timestamp} ]);
             });
         })
-    
+
+        // User-submitted transaction
+        DOM.elid('submit-KD1234').addEventListener('click', () => {
+            let value = DOM.elid('flight-KD1234').value;
+            let flight = "KD1234";
+            contract.registerFlight(flight, value, (error, result) => {
+               display('Insurance', 'Flight '+ flight, [ { label: 'Purchased Insurance!', error: error, value: result} ]);
+            });
+        })
+
+        DOM.elid('submit-KD5678').addEventListener('click', () => {
+            let value = DOM.elid('flight-KD5678').value;
+            let flight = "KD5678";
+            contract.registerFlight(flight, value, (error, result) => {
+                display('Insurance', 'Flight '+ flight, [ { label: 'Purchased Insurance!', error: error, value: result} ]);
+            });
+        })
+
+        DOM.elid('submit-KD8888').addEventListener('click', () => {
+            let value = DOM.elid('flight-KD8888').value;
+            let flight = "KD8888";
+            contract.registerFlight(flight, value, (error, result) => {
+                display('Insurance', 'Flight '+ flight, [ { label: 'Purchased Insurance!', error: error, value: result} ]);
+            });
+        })
+
+
     });
-    
+
 
 })();
 
@@ -46,10 +75,3 @@ function display(title, description, results) {
     displayDiv.append(section);
 
 }
-
-
-
-
-
-
-
